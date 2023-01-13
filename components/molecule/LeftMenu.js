7511f622d0d7476b6b6e74e61b6ffb10bec11471
Dolgoon <83904398+dolgoonnn@users.noreply.github.com>
2023-01-13 16:37:02 +0800

    left menu fix

--- components/molecule/LeftMenu.js
+++ components/molecule/LeftMenu.js
@@ -13,6 +13,7 @@ import { useRouter } from "next/router";
 import Image from "next/image";
 
 export default function LeftMenu({ detail, id, course }) {
+    console.log("🚀 ~ file: LeftMenu.js:16 ~ LeftMenu ~ detail", detail);
     console.log("🚀 ~ file: LeftMenu.js:16 ~ LeftMenu ~ course", course);
 
     const router = useRouter();
@@ -43,6 +44,7 @@ export default function LeftMenu({ detail, id, course }) {
                     <h3>Overview</h3>
                 </a>
                 {detail &&
+                    typeof detail === "object" &&
                     Object.keys(detail).map((chapter) => (
                         <div key={chapter}>
                             <h3>{chapter}</h3>
@@ -52,39 +54,41 @@ export default function LeftMenu({ detail, id, course }) {
                                 size={4}
                                 className="pt-4"
                             >
-                                {detail[chapter].map((child) => (
-                                    <a
-                                        key={child.id}
-                                        onClick={() => {
-                                            router.push({
-                                                pathname:
-                                                    "/builds/[id]/sub-courses/[subCourseID]",
-                                                query: {
-                                                    id,
-                                                    subCourseID: child.id,
-                                                },
-                                            });
-                                        }}
-                                    >
-                                        <Space>
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                fill="none"
-                                                viewBox="0 0 24 24"
-                                                strokeWidth={3}
-                                                stroke="currentColor"
-                                                className="w-4 h-4"
-                                            >
-                                                <path
-                                                    strokeLinecap="round"
-                                                    strokeLinejoin="round"
-                                                    d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
-                                                />
-                                            </svg>
-                                            {child.subCourseName}
-                                        </Space>
-                                    </a>
-                                ))}
+                                {typeof detail === "object" &&
+                                    Array.isArray(detail[chapter]) &&
+                                    detail[chapter].map((child) => (
+                                        <a
+                                            key={child.id}
+                                            onClick={() => {
+                                                router.push({
+                                                    pathname:
+                                                        "/builds/[id]/sub-courses/[subCourseID]",
+                                                    query: {
+                                                        id,
+                                                        subCourseID: child.id,
+                                                    },
+                                                });
+                                            }}
+                                        >
+                                            <Space>
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    fill="none"
+                                                    viewBox="0 0 24 24"
+                                                    strokeWidth={3}
+                                                    stroke="currentColor"
+                                                    className="w-4 h-4"
+                                                >
+                                                    <path
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
+                                                    />
+                                                </svg>
+                                                {child.subCourseName}
+                                            </Space>
+                                        </a>
+                                    ))}
                             </Space>
                         </div>
                     ))}
