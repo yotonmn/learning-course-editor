67ccd6f45ad2fa85debb3f3392dad4861d3279a8
Dolgoon <83904398+dolgoonnn@users.noreply.github.com>
2023-01-17 11:42:52 +0800

    admin fix

--- pages/builds/[id]/sub-courses/[subCourseId].js
+++ pages/builds/[id]/sub-courses/[subCourseId].js
@@ -183,7 +183,11 @@ export default function Detail() {
                 <div className="h-full border-t border-trueGray-700 ">
                     <div className="h-full container mx-auto flex px-2">
                         {/* {course?.course?.courseName} */}
-                        <LeftMenu detail={course?.subCourses} id={id} />
+                        <LeftMenu
+                            detail={course?.subCourses}
+                            course={course?.course}
+                            id={id}
+                        />
                         <div className="w-full p-8 max-h-screen overflow-y-auto">
                             <Breadcrumb
                                 courseName={course?.course?.courseName}
